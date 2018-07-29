238651c9e1b2ee6fdf32b4df34e93528b85abc0f
cscal <35845978+cscal@users.noreply.github.com>
2018-07-29 19:46:28 -0400

    update recover.c
    First version that compiles successfully, however output still has an unexpected bug.

--- recover.c
+++ recover.c
@@ -1,25 +1,4 @@
-/*
- * recover.c
- * 
- * Copyright 2018  <pi@raspberrypi>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
- * 
- */
+
 // Recovers JPG images from a file of raw data
 
 #include <stdio.h>
@@ -33,23 +12,68 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Usage: recover infile\n");
         return 1;
     }
-    
+
+    char *infile = argv[1];
+
     // open input file
-    FILE *infile = fopen(argv[1], "r");
-    if (infile == NULL)
+    FILE *inptr = fopen(infile, "r");
+    if (inptr == NULL)
     {
         fprintf(stderr, "Could not open %s.\n", infile);
         return 2;
     }
-    
-    // open output file
-    //FILE *outfile = fopen(outfile, "w");
-    
-    typedef struct
+
+    // Declare variables
+    char filename[8];
+    int file = 1;
+    unsigned char buffer[512];
+    int start = 0;
+    sprintf(filename, "%03i.jpg", file);
+    FILE* img = fopen(filename, "w");
+
+    // While fread successfully returns 512 bytes, indicating we have not yet reached the end of the file...
+    while (fread(&buffer, 512, 1, inptr) == 1)
     {
-		
-	};
-    
+        // Read into buffer and compare the first 4 bytes of the buffer with the JPG header signature
+        fread(&buffer, 512, 1, inptr);
+            // This finds the first JPG, allowing the loop to throw away garbage data at the beginning of the raw file
+            if (start == 0 &&
+                buffer[0] == 0xff &&
+                buffer[1] == 0xd8 &&
+                buffer[2] == 0xff &&
+                (buffer[3] & 0xf0) == 0xe0)
+            {
+                // Header matches, so write buffer to file and set Start of JPGs to true (1)
+                fwrite(&buffer, 512, 1, img);
+                start = 1;
+            }
+            // If we've already writing out to a JPG, continue doing so until another header is reached
+            else if (start == 1 &&
+                buffer[0] != 0xff &&
+                buffer[1] != 0xd8 &&
+                buffer[2] != 0xff &&
+                (buffer[3] & 0xf0) != 0xe0)
+            {
+                fopen(filename, "a");
+                fwrite(&buffer, 512, 1, img);
+            }
+            // This finds the 2nd and succeeding JPG image headers, where it needs to close the preceding image,
+            // increment the file number and update the filename to open, then start writing in the new file.
+            else if (start == 1 &&
+                buffer[0] == 0xff &&
+                buffer[1] == 0xd8 &&
+                buffer[2] == 0xff &&
+                (buffer[3] & 0xf0) == 0xe0)
+            {
+                fclose(img);
+                file++;
+                sprintf(filename, "%03i.jpg", file);
+                img = fopen(filename, "w");
+                fwrite(&buffer, 512, 1, img);
+            }
+    }
+
+    fclose(img);
+    fclose(inptr);
 	return 0;
 }
-
